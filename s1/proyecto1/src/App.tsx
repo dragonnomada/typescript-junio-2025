9793a5e735a780ea201c1b94231d7ac67dbbac03
Alan Badillo Salas <dragonnomada123@gmail.com>
2025-06-25 10:25:43 -0600

    Proyecto 1

---
+++ s1/proyecto1/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+
+    // Win + .  ->  Ventana de íconos
+    // ctrl + cmd + space  ->  Ventana de íconos
+    return (
+        <span>Hola react ⚛️</span>
+    )
+
+}
