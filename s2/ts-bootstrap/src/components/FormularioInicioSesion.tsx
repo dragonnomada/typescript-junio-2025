5c661a65d477d2131ea8a91679110120a34b4ae8
Alan Badillo Salas <dragonnomada123@gmail.com>
2025-06-26 10:52:39 -0600

    Sesión 2 - Formulario de Inicio de Sesión

--- s2/ts-bootstrap/src/components/FormularioInicioSesion.tsx
+++ s2/ts-bootstrap/src/components/FormularioInicioSesion.tsx
@@ -1,13 +1,61 @@
-import { Col, Container, Row } from "react-bootstrap"
+import { faLock } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Button, Col, Container, Form, Image, Row } from "react-bootstrap"
 
 export default function FormularioInicioSesion() {
 
     return (
-        <Container>
-            <Row>
-                <Col xs={12} sm={8} md={6} className="bg-primary">A</Col>
-                <Col xs={6} sm={4} md={3} className="bg-success">B</Col>
-                <Col xs={6} sm={12} md={3} className="bg-danger">C</Col>
+        <Container
+            className="w-100 h-100 p-0"
+            fluid
+        >
+            <Row
+                className="w-100 h-100 bg-light"
+            >
+                <Col
+                    className="p-0"
+                    xs={12}
+                    md={6}
+                >
+                    <Image
+                        className="w-100 h-100 object-fit-cover"
+                        src="https://wallpapers.com/images/hd/business-city-view-tt8vcogkrf94giby.jpg"
+                        fluid
+                    />
+                </Col>
+                <Col
+                    xs={12}
+                    md={6}
+                >
+                    <Form
+                        className="d-flex flex-column gap-3 p-3"
+                        onSubmit={(event) => {
+                            event.preventDefault()
+                        }}
+                    >
+                        <h1>Bienvenido</h1>
+                        <h2>Iniciar Sesión</h2>
+                        <Form.Group>
+                            <Form.Label>Correo</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Ej. alicia@gmail.com"
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Ej. •••••"
+                            />
+                        </Form.Group>
+                        <Button
+                            type="submit"
+                        >
+                            Iniciar Sesión <FontAwesomeIcon icon={faLock} />
+                        </Button>
+                    </Form>
+                </Col>
             </Row>
         </Container>
     )
